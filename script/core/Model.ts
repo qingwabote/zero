a0669805732f3b9cde936bc0f899b2c61f79731f
LoganLiu <qingwabote@126.com>
2022-07-15 21:01:34 +0800

    mat4: translation, rotation of axes and multiply

--- script/core/Model.ts
+++ script/core/Model.ts
@@ -1,8 +1,8 @@
-import { mat4, vec3 } from "gl-matrix";
 import Buffer, { BufferUsageBit } from "./gfx/Buffer.js";
 import gfx, { Transform } from "./gfx.js";
 import { DescriptorSet, BuiltinUniformBlocks, BuiltinDescriptorSetLayouts } from "./gfx/Pipeline.js";
 import SubModel from "./SubModel.js";
+import mat4 from "./math/mat4.js";
 
 export default class Model {
     private _descriptorSet: DescriptorSet;
@@ -27,11 +27,22 @@ export default class Model {
     }
 
     update() {
-        const matWorld = mat4.create();
-        mat4.translate(matWorld, matWorld, vec3.fromValues(this._transform.x, this._transform.y, this._transform.z))
-        mat4.rotateX(matWorld, matWorld, Math.PI / 180 * this._transform.eulerX);
-        mat4.rotateY(matWorld, matWorld, Math.PI / 180 * this._transform.eulerY);
-        mat4.rotateZ(matWorld, matWorld, Math.PI / 180 * this._transform.eulerZ);
-        this._descriptorSet.buffers[BuiltinUniformBlocks.local.blocks.Local.binding].update(matWorld as Float32Array);
+        // const matWorld = mat4.create();
+        // mat4.translate(matWorld, matWorld, vec3.fromValues(this._transform.x, this._transform.y, this._transform.z))
+        // mat4.rotateX(matWorld, matWorld, Math.PI / 180 * this._transform.eulerX);
+        // mat4.rotateY(matWorld, matWorld, Math.PI / 180 * this._transform.eulerY);
+        // mat4.rotateZ(matWorld, matWorld, Math.PI / 180 * this._transform.eulerZ);
+
+        const rx = mat4.fromXRotation(Math.PI / 180 * this._transform.eulerX);
+        const ry = mat4.fromYRotation(Math.PI / 180 * this._transform.eulerY);
+        const rz = mat4.fromZRotation(Math.PI / 180 * this._transform.eulerZ);
+        const r = mat4.multiply(mat4.create(), rz, mat4.multiply(mat4.create(), ry, rx));
+        // const matWorld = mat4.translate(mat4.create(), rx, this._transform.x, this._transform.y, this._transform.z);
+        const t = mat4.fromTranslation(this._transform.x, this._transform.y, this._transform.z);
+        const matWorld = mat4.multiply(mat4.create(), t, r)
+
+
+
+        this._descriptorSet.buffers[BuiltinUniformBlocks.local.blocks.Local.binding].update(new Float32Array(matWorld));
     }
 }
---
+++ script/core/math/mat4.ts
@@ -0,0 +1,117 @@
+//right-handed
+
+export type Mat4 = [
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number
+]
+
+
+export default {
+    create(): Mat4 {
+        return [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+    },
+
+    fromXRotation(radian: number): Mat4 {
+        const s = Math.sin(radian);
+        const c = Math.cos(radian);
+
+        return [
+            1, 0, 0, 0,
+            0, c, s, 0,
+            0, -s, c, 0,
+            0, 0, 0, 1
+        ];
+    },
+
+    fromYRotation(radian: number): Mat4 {
+        const s = Math.sin(radian);
+        const c = Math.cos(radian);
+
+        return [
+            c, 0, -s, 0,
+            0, 1, 0, 0,
+            s, 0, c, 0,
+            0, 0, 0, 1
+        ];
+    },
+
+    fromZRotation(radian: number): Mat4 {
+        const s = Math.sin(radian);
+        const c = Math.cos(radian);
+
+        return [
+            c, s, 0, 0,
+            -s, c, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ];
+    },
+
+    fromTranslation(x: number, y: number, z: number): Mat4 {
+        return [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            x, y, z, 1
+        ]
+    },
+
+    translate(out: Mat4, a: Mat4, x: number, y: number, z: number): Mat4 {
+        if (out != a) {
+            out[0] = a[0];
+            out[1] = a[1];
+            out[2] = a[2];
+            out[3] = a[3];
+            out[4] = a[4];
+            out[5] = a[5];
+            out[6] = a[6];
+            out[7] = a[7];
+            out[8] = a[8];
+            out[9] = a[9];
+            out[10] = a[10];
+            out[11] = a[11];
+
+            out[15] = a[15];
+        }
+
+        out[12] += x;
+        out[13] += y;
+        out[14] += z;
+        return out;
+    },
+
+    multiply(out: Mat4, a: Mat4, b: Mat4): Mat4 {
+        let [x, y, z, w] = [b[0], b[1], b[2], b[3]];
+        out[0] = x * a[0] + y * a[4] + z * a[8] + w * a[12];
+        out[1] = x * a[1] + y * a[5] + z * a[9] + w * a[13];
+        out[2] = x * a[2] + y * a[6] + z * a[10] + w * a[14];
+        out[3] = x * a[3] + y * a[7] + z * a[11] + w * a[15];
+
+        [x, y, z, w] = [b[4], b[5], b[6], b[7]];
+        out[4] = x * a[0] + y * a[4] + z * a[8] + w * a[12];
+        out[5] = x * a[1] + y * a[5] + z * a[9] + w * a[13];
+        out[6] = x * a[2] + y * a[6] + z * a[10] + w * a[14];
+        out[7] = x * a[3] + y * a[7] + z * a[11] + w * a[15];
+
+        [x, y, z, w] = [b[8], b[9], b[10], b[11]];
+        out[8] = x * a[0] + y * a[4] + z * a[8] + w * a[12];
+        out[9] = x * a[1] + y * a[5] + z * a[9] + w * a[13];
+        out[10] = x * a[2] + y * a[6] + z * a[10] + w * a[14];
+        out[11] = x * a[3] + y * a[7] + z * a[11] + w * a[15];
+
+        [x, y, z, w] = [b[12], b[13], b[14], b[15]];
+        out[12] = x * a[0] + y * a[4] + z * a[8] + w * a[12];
+        out[13] = x * a[1] + y * a[5] + z * a[9] + w * a[13];
+        out[14] = x * a[2] + y * a[6] + z * a[10] + w * a[14];
+        out[15] = x * a[3] + y * a[7] + z * a[11] + w * a[15];
+
+        return out;
+    }
+}
