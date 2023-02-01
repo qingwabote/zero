841ad91c335429d03bb996b2b996d11a5a2678a6
Logan <qingwabote@126.com>
2023-02-01 18:04:25 +0800

    add ammo.js

--- script/core/physics/PhysicsSystem.ts
+++ script/core/physics/PhysicsSystem.ts
@@ -1,4 +1,4 @@
-import initModule from "./impl/helloworld.js";
+import Ammo from "./impl/ammo.wasm.js";
 
 export default class PhysicsSystem {
     private static _instance: PhysicsSystem;
@@ -9,7 +9,7 @@ export default class PhysicsSystem {
 
     static async initialize(): Promise<void> {
         const Module: any = {}
-        Module.wasmBinary = await loader.load('../../asset/physics/helloworld.wasm', 'arraybuffer');
+        Module.wasmBinary = await loader.load('../../asset/physics/ammo.wasm.wasm', 'arraybuffer');
         Module.printErr = console.log.bind(console);
         Module.locateFile = function () { return 'not care' };
 
@@ -17,13 +17,22 @@ export default class PhysicsSystem {
         if (typeof globalThis.window != 'object') {
             (globalThis as any).window = emptyObj;
         }
-        await initModule(Module);
+        await Ammo(Module);
         if ((globalThis as any).window == emptyObj) {
             (globalThis as any).window = undefined;
         }
+        PhysicsSystem._instance = new PhysicsSystem(Module);
+    }
 
-        let foo = new Module.Foo();
-        console.log("foo.getVal()", foo.getVal());
-        PhysicsSystem._instance = new PhysicsSystem;
+    private _broadphase;
+    private _dispatcher;
+    private _solver;
+    private _world;
+
+    constructor(readonly ammo: any) {
+        this._broadphase = new ammo.btDbvtBroadphase();
+        this._dispatcher = new ammo.btCollisionDispatcher(new ammo.btDefaultCollisionConfiguration());
+        this._solver = new ammo.btSequentialImpulseConstraintSolver();
+        this._world = new ammo.btDiscreteDynamicsWorld(this._broadphase, this._dispatcher, this._solver);
     }
 }
