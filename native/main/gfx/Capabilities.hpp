5bfb0cf2717bdd1146c4598b893fcfdd860bdb82
Logan <qingwabote@126.com>
2025-02-23 18:56:38 +0800

    refactor

--- native/main/gfx/Capabilities.hpp
+++ native/main/gfx/Capabilities.hpp
@@ -7,10 +7,10 @@ namespace gfx
     class Capabilities
     {
     public:
-        const uint32_t uniformBufferOffsetAlignment;
+        const size_t uniformBufferOffsetAlignment;
         const int clipSpaceMinZ;
 
-        Capabilities(uint32_t uniformBufferOffsetAlignment, int clipSpaceMinZ)
+        Capabilities(size_t uniformBufferOffsetAlignment, int clipSpaceMinZ)
             : uniformBufferOffsetAlignment(uniformBufferOffsetAlignment),
               clipSpaceMinZ(clipSpaceMinZ) {}
     };
