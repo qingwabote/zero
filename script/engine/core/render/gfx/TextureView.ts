4a0fc511c5730da10b48693f5e162db6b7abb37e
Logan <qingwabote@126.com>
2025-06-27 20:18:23 +0800

    optimize size of TextureView

--- script/engine/core/render/gfx/TextureView.ts
+++ script/engine/core/render/gfx/TextureView.ts
@@ -1,40 +1,51 @@
 import { device } from "boot";
 import { CommandBuffer, Format, Texture, TextureInfo, TextureUsageFlagBits } from "gfx";
+import { vec2, Vec2 } from "../../math/vec2.js";
 import { MemoryView } from "../gfx/MemoryView.js";
 
-function length2extent(length: number): number {
+const vec2_a = vec2.create();
+
+function length2extent(out: Vec2, length: number) {
+    if (length == 0) {
+        out[0] = 0;
+        out[1] = 0;
+        return out;
+    }
+
     const texels = Math.ceil(length / 4);
-    const extent = Math.ceil(Math.sqrt(texels));
-    const n = Math.ceil(Math.log2(extent));
-    return Math.pow(2, n);
+    const width = 1 << Math.ceil(Math.log2(Math.ceil(Math.sqrt(texels))));
+    const height = 1 << Math.ceil(Math.log2(Math.ceil(texels / width)));
+    out[0] = width;
+    out[1] = height;
+    return out;
 }
 
 export class TextureView extends MemoryView {
     readonly texture: Texture;
 
     constructor(length: number = 0) {
-        const extent = length2extent(length);
-        super('f32', length, extent * extent * 4)
+        const [width, height] = length2extent(vec2_a, length);
+        super('f32', length, width * height * 4)
 
         const info = new TextureInfo;
         info.format = Format.RGBA32_SFLOAT;
         info.usage = TextureUsageFlagBits.SAMPLED | TextureUsageFlagBits.TRANSFER_DST;
-        info.width = info.height = extent;
+        info.width = width;
+        info.height = height;
         this.texture = device.createTexture(info);
     }
 
     public override reserve(min: number) {
-        const extent = length2extent(min);
-        const capacity = extent * extent * 4;
-        return super.reserve(capacity);
+        const [width, height] = length2extent(vec2_a, min);
+        return super.reserve(width * height * 4);
     }
 
     protected override upload(commandBuffer: CommandBuffer, offset: number, length: number) {
-        const extent = length2extent(this.length);
-        let width = this.texture.info.width;
-        if (width < extent) {
-            this.texture.resize(extent, extent);
-            width = extent;
+        let { width, height } = this.texture.info;
+        const extent = length2extent(vec2_a, this.length);
+        if (width * height < extent[0] * extent[1]) {
+            this.texture.resize(extent[0], extent[1]);
+            width = extent[0];
             offset = 0;
             length = this.length;
         }
